18d9d80b55a2fe40ba80c5bfe951be704436977a
brunodobasico <83381105+brunodobasico@users.noreply.github.com>
2025-05-22 15:12:33 +0100

    algumas alteraçoes

---
+++ A-Mover-Desktop-Final/A-Mover-Desktop-Final/Data/Migrations/20250522132701_atualziarmodelservico.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace A_Mover_Desktop_Final.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class atualziarmodelservico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NotasServico",
+                table: "Servico",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotasServico",
+                table: "Servico");
+        }
+    }
+}
